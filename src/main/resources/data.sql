8e2c5e86653e788c45dd22d14222cd9a48c0f283
hayakawa <hayakawatakahiko@gmail.com>
2019-08-29 23:37:49 +0900

    db設定

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+/** 従業員データ**/
+INSERT INTO employee (employee_id, employee_name, age) VALUES (1, '山田太郎',30);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+/** 従業員**/
+CREATE TABLE IF NOT EXISTS employee (
+	employee_id INT PRIMARY KEY,
+	employee_name VARCHAR(50),
+	age INT
+);
