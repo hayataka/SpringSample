7f39a25537465e44d14a1af9233fb56219294783
hayakawa <hayakawatakahiko@gmail.com>
2019-08-29 23:20:05 +0900

    3-2まで、画面からサーバへ、また画面に戻す

--- src/main/java/com/example/demo/trySpring/HelloController.java
+++ src/main/java/com/example/demo/trySpring/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.trySpring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,6 +12,14 @@ public class HelloController {
 	@GetMapping
 	public String getHello() {
 		return "hello";
+	}
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, Model model) {
+		
+		model.addAttribute("sample", str);
+		
+		return "helloResponse";
 	}
 	
 	
