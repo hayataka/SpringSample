45d46def4ede82e160cb10c563ab80527f88f255
hayakawa <hayakawatakahiko@gmail.com>
2019-08-31 17:05:18 +0900

    7-3-1まで、logだけslfj4

---
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -0,0 +1,24 @@
+package com.example.demo.login.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LogAspect {
+
+	@Before("execution( * com.example.demo.login.controller.LoginController.getLogin(..))")
+	public void startLog(JoinPoint jp) {
+		log.debug("メソッド開始：{}", jp.getSignature());
+	}
+	@After("execution( * com.example.demo.login.controller.LoginController.getLogin(..))")
+	public void endLog(JoinPoint jp) {
+		log.debug("メソッド終了:{}", jp.getSignature());
+	}
+}
