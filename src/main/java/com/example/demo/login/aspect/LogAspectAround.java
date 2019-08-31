4068e726bb5fe511608e6b0e7e63357678f52f86
hayakawa <hayakawatakahiko@gmail.com>
2019-08-31 17:48:10 +0900

    ログ出し過ぎのパターンをtraceで抑止

--- src/main/java/com/example/demo/login/aspect/LogAspectAround.java
+++ src/main/java/com/example/demo/login/aspect/LogAspectAround.java
@@ -1,11 +1,8 @@
 package com.example.demo.login.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,14 +15,14 @@ public class LogAspectAround {
 	@Around("execution( * *..*.*Controller.*(..))")
 	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
 
-		log.debug("メソッド開始：{}", jp.getSignature());
+		log.trace("メソッド開始：{}", jp.getSignature());
 		try {
 			Object result = jp.proceed();
-			log.debug("メソッド終了：{}", jp.getSignature());
+			log.trace("メソッド終了：{}", jp.getSignature());
 			return result;
 		} catch(Exception e) {
-			log.error("メソッド異常終了:{}", jp.getSignature());
-			log.error("エラーが発生しました", e);
+			log.trace("メソッド異常終了:{}", jp.getSignature());
+			log.trace("エラーが発生しました", e);
 			throw e;
 		}
 	}
