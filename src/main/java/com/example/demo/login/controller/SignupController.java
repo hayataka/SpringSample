ad719d0d7733c19a1d4169d7ab571d948b7becff
hayakawa <hayakawatakahiko@gmail.com>
2019-08-31 16:03:39 +0900

    validation

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +35,7 @@ public class SignupController {
 	}
 	
 	@PostMapping("/signup")
-	public String postSignUp(@ModelAttribute SignupForm form, BindingResult bindingResult, Model model) {
+	public String postSignUp(@ModelAttribute @Validated SignupForm form, BindingResult bindingResult, Model model) {
 		
 		if(bindingResult.hasErrors()) {
 			return getSignUp(form, model);
--- src/main/java/com/example/demo/login/domain/model/SignupForm.java
+++ src/main/java/com/example/demo/login/domain/model/SignupForm.java
@@ -2,6 +2,15 @@ package com.example.demo.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -9,11 +18,21 @@ import lombok.Data;
 @Data
 public class SignupForm {
 
+	@NotBlank
+	@Email
 	private String userId;
+	@NotBlank
+	@Length(min=4, max=100)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String password;
+	@NotBlank
 	private String userName;
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@NotNull
 	private Date birthday;
+	@Min(20)
+	@Max(100)
 	private int age;
+	@AssertFalse
 	private boolean marriage;
 }
