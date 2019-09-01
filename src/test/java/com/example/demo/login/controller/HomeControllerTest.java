4264037f89bbca79a38e085e6da4d27cb1df0a5d
hayakawa <hayakawatakahiko@gmail.com>
2019-09-01 14:35:23 +0900

    12 画面のテスト

---
+++ src/test/java/com/example/demo/login/controller/HomeControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.demo.login.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.demo.login.domain.service.UserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private UserService service;
+
+	@Test
+	@WithMockUser
+	public void test() throws Exception {
+		
+		when(service.count()).thenReturn(10);
+
+		mockMvc.perform(get("/userList"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("合計：10件")));
+		
+	}
+
+}
---
+++ src/test/java/com/example/demo/login/controller/LoginControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.demo.login.controller;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(get("/login"))
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("ユーザーID")));
+	}
+
+}
