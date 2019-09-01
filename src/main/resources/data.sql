4509ff69648131186786b17b7e8828a785609e8e
hayakawa <hayakawatakahiko@gmail.com>
2019-09-01 11:23:46 +0900

    10.2 セキュリティ　認証処理作成前

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO employee (employee_id, employee_name, age) VALUES (1, '山田太郎'
 /* ユーザーマスタのデータ（ADMIN権限） */
 INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
 VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
+
+
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('tamura@xxx.co.jp', 'password', '田', '1990-01-01', 28, false, 'ROLE_GENERAL');
