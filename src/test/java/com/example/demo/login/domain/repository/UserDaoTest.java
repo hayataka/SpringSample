c55c07ce4b195f13610ee1d165508be60bb1b6a9
hayakawa <hayakawatakahiko@gmail.com>
2019-09-01 14:12:55 +0900

    (任意で良いですが）書籍と異なり、src/main/java側と同じパッケージ階層にテストクラスを作成

---
+++ src/test/java/com/example/demo/login/domain/repository/UserDaoTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.login.domain.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
