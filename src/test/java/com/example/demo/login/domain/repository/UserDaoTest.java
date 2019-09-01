01ecf3c1a959eed1985499ae35899ea9eb2e1355
hayakawa <hayakawatakahiko@gmail.com>
2019-09-01 14:21:24 +0900

    12.3 daoのテスト　・・・ spring-testの中に@Sqlアノテーションがあった

--- src/test/java/com/example/demo/login/domain/repository/UserDaoTest.java
+++ src/test/java/com/example/demo/login/domain/repository/UserDaoTest.java
@@ -1,14 +1,41 @@
 package com.example.demo.login.domain.repository;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class UserDaoTest {
 
+	@Autowired
+	@Qualifier("UserDaoJdbcImpl")
+	UserDao dao;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCount() {
+		int count = dao.count();
+		assertEquals(2, count);
+		// 記述はこちらの方が好き
+		assertThat(count, is(2));
+
+	}
+
+	@Test
+	@Sql("/testdata.sql")   // Spring-test jar
+	public void testCount_afterSql() {
+		int count = dao.count();
+		assertThat(count, is(3));
+
 	}
 
 }
---
+++ src/test/resources/testdata.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('satou@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '佐藤一郎', '1990-01-02', 29, false, 'ROLE_ADMIN');
+
