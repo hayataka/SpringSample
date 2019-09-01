ff06e29c3a42e46157126eb66159d58f2fc9e202
hayakawa <hayakawatakahiko@gmail.com>
2019-09-01 10:36:59 +0900

    9.4 Controller内での例外aspect

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -4,10 +4,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,4 +67,20 @@ public class SignupController {
 		
 		return "redirect:/login";
 	}
+	
+	@ExceptionHandler(DataAccessException.class)
+	public String dataAccessExceptionHandler(DataAccessException e, Model model) {
+		model.addAttribute("error", "内部サーバーエラー：ExceptionHandler");
+		model.addAttribute("message", "SignupControllerでDataAccessExceptionが発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		return "error";
+	}
+
+	@ExceptionHandler(Exception.class)
+	public String exceptionHandler(DataAccessException e, Model model) {
+		model.addAttribute("error", "内部サーバーエラー：ExceptionHandler");
+		model.addAttribute("message", "SignupControllerでExceptionが発生しました");
+		model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR);
+		return "error";
+	}	
 }
